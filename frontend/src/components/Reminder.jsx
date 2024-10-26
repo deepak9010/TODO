254f27a3203485cafa1817b3cb23c6925a964175
deepak9010 <deepak23072019@gmail.com>
2024-10-27 01:17:49 +0530

    reminder api

--- frontend/src/components/Reminder.jsx
+++ frontend/src/components/Reminder.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa'; 
 import '../styles/reminder.css';
 
+console.log('API URL:', `${process.env.REACT_APP_API_URL}/pending-count`);
+
 const Reminder = () => {
-    const totalTasks = 10;
-    const completedTasks = 6; 
-    const progress = (completedTasks / totalTasks) * 100;
+  const [completedTasks, setCompletedTasks] = useState(0);
+  const [pendingTasks, setPendingTasks] = useState(0);
+  const totalTasks = completedTasks + pendingTasks; 
+  const progress = totalTasks ? (completedTasks / totalTasks) * 100 : 0;
+
+  useEffect(() => {
+    // Fetch completed tasks count
+    const fetchCompletedTasks = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/completed-count`);
+        const data = await response.json();
+        setCompletedTasks(data.completed_count); 
+        // console.log('Completed Tasks:', data.completed_count);
+      } catch (error) {
+        console.error('Error fetching completed tasks:', error);
+      }
+    };
+
+    // Fetch pending tasks count
+    const fetchPendingTasks = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/pending-count`);
+        const data = await response.json();
+        setPendingTasks(data.pending_count);
+        // console.log('Pending Tasks:', data.pending_count);  
+      } catch (error) {
+        console.error('Error fetching pending tasks:', error);
+      }
+    };
+
+    fetchCompletedTasks();
+    fetchPendingTasks();
+  }, []);
+
+
   return (
     <>
       <div className="task-status mt-4">
@@ -15,7 +49,7 @@ const Reminder = () => {
             <span className="task-text">Task Complete</span>
           </div>
           <div className="task-number-info d-flex align-items-center">
-            <span className="task-number">5</span> 
+            <span className="task-number">{completedTasks}</span> 
             <span className="task-week">This Week</span>
           </div>
         </div>
@@ -26,7 +60,7 @@ const Reminder = () => {
             <span className="task-text">Task Incomplete</span>
           </div>
           <div className="task-number-info d-flex align-items-center">
-            <span className="task-number">5</span> 
+            <span className="task-number">{pendingTasks}</span> 
             <span className="task-week">This Week</span>
           </div>
         </div>
